b22a15b270f2997882c6398fcdd88cf098fbe62c
muaxz <bexsd@hotmail.com>
2021-02-07 14:11:58 +0300

    new

--- client/pages/login.js
+++ client/pages/login.js
@@ -128,7 +128,7 @@ const Login=()=>{
        <ImageDiv urlget={currenturl}>
            <Global/>
             <Head>
-              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossOrigin="anonymous" />
               <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Font+Name"/>
             </Head>
            <WindowDiv>
