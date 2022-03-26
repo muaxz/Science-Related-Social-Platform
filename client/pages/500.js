3e81dbf73c3d4a7040e1b3720f10b7cd6c46583e
muaxz <bexsd@hotmail.com>
2022-03-26 02:42:11 +0100

    change

--- client/pages/500.js
+++ client/pages/500.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Custom500() {
     return (
         <div>
-            500 error occured!
+            500 error occured! Updated page
         </div>
     )
 }
