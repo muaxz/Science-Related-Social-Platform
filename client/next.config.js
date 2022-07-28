7931c67fe3d209e1c7c631ff5f1a2b47da0b1654
muaxz <bexsd@hotmail.com>
2022-07-28 19:56:04 -0400

    change

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     async headers(){
       return [
         {
-          soruce:"/:path",
+          source:"/",
           headers:[
             {key:"Access-Control-Allow-Credentials",value:"true"}
           ]
