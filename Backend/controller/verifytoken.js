4ea895ad9563fc4f95b6cd5d9d35cb9491713140
muaxz <bexsd@hotmail.com>
2022-07-27 20:18:30 -0400

    change

--- Backend/controller/verifytoken.js
+++ Backend/controller/verifytoken.js
@@ -1,7 +1,14 @@
 const jwt=require("jsonwebtoken");
-const redis = require("redis")
+const Redis = require("ioredis")
 
-const client = redis.createClient();
+const client = new Redis({
+    host:"ec2-44-199-54-215.compute-1.amazonaws.com",
+    port:7970,
+    password:"p02f1a4d23900a9697de8339827683c998e2bb370467e9070a9353cf970871c36",
+    tls:{
+       rejectUnauthorized: false
+    }
+ })
 
 client.connect().then(()=>console.log("connecnted to redis server"))
 
