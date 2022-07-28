45b78fde834e25e62c05a9c443ca97971c3d897f
muaxz <bexsd@hotmail.com>
2022-07-28 19:47:24 -0400

    change

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,17 @@
 module.exports = {
+    async headers(){
+      return [
+        {
+          soruce:"/",
+          headers:[
+            {key:"Access-Control-Allow-Credentials",value:true}
+          ]
+        }
+      ]
+    },
     compiler: {
       // ssr and displayName are configured by default
       styledComponents: true,
     },
-    
-}
+
+  }
