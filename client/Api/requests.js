ac2bdbddfb2ae80768313eef72ff8807085c3d7f
muaxz <bexsd@hotmail.com>
2022-08-01 04:05:18 -0400

    change

--- client/Api/requests.js
+++ client/Api/requests.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "next/router"
 
 
-axios.defaults.baseURL="https://ideasharee.herokuapp.com";
+axios.defaults.baseURL="https://mynextrepo.vercel.app";
 
 axios.defaults.withCredentials = true;
 
