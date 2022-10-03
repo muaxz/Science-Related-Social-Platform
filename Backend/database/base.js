39c95927927351d221f7cc697df4b50aff3ca23d
muaxz <bexsd@hotmail.com>
2022-10-03 12:24:33 -0400

    change

--- Backend/database/base.js
+++ Backend/database/base.js
@@ -4,7 +4,7 @@ const Sequlize=require("sequelize");
 const sequlize=new Sequlize("heroku_9a64dbd4cf1fbab","ba618bc640d60e","24a1e46f",{
     dialect:"mysql",
     host:"us-cdbr-east-06.cleardb.net",
-    port:3306
+    port:3306,
 })
 
 sequlize.authenticate()
