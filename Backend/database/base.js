b4612ba7110a3546d153c2ba6e433d2f164e2502
muaxz <bexsd@hotmail.com>
2022-10-03 12:30:18 -0400

    change

--- Backend/database/base.js
+++ Backend/database/base.js
@@ -5,6 +5,9 @@ const sequlize=new Sequlize("heroku_9a64dbd4cf1fbab","ba618bc640d60e","24a1e46f"
     dialect:"mysql",
     host:"us-cdbr-east-06.cleardb.net",
     port:3306,
+    pool:{
+        max:100
+    }
 })
 
 sequlize.authenticate()
