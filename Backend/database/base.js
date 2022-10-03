8b46eaa51a04463d457243de3615fa0b2ce9c8ed
muaxz <bexsd@hotmail.com>
2022-10-03 13:10:54 -0400

    change

--- Backend/database/base.js
+++ Backend/database/base.js
@@ -5,8 +5,11 @@ const sequlize=new Sequlize("heroku_9a64dbd4cf1fbab","ba618bc640d60e","24a1e46f"
     dialect:"mysql",
     host:"us-cdbr-east-06.cleardb.net",
     port:3306,
-    pool:{
-        max:100
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 60000
     }
 })
 
